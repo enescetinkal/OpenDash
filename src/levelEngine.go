7ab9029158ee25601302b357198e1865b3760f44
enescetinkal <151547869+enescetinkal@users.noreply.github.com>
2026-02-19 21:28:28 +0300

    level loading

--- src/levelEngine.go
+++ src/levelEngine.go
@@ -1,6 +1,32 @@
 package main
 
+import (
+	"os"
+	"time"
+	"encoding/json"
+)
 
-func InitalizeLevel(filepath string){
+type CondensedObject struct{
+	x float32
+	y float32
+	rotation float32
+	id uint
+	depth int8
+}
 
+type Level struct{
+	name string
+	updateDate time.Time
+	objects []LevelObject
+}
+
+func LoadLevel(filename string) (Level, error) {
+    var lvl Level
+
+    data, err := os.ReadFile(filename)
+    if err != nil {
+        return lvl, err
+    }
+    err = json.Unmarshal(data, &lvl)
+    return lvl, err
 }
